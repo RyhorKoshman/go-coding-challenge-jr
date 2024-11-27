d333f6c4e73e59031977907cd64114367b7dc835
Ryhor Koshman <ryhor.koshman@gmail.com>
2024-11-27 01:27:00 +0100

    Corrected MakeShortLink func and added simple client realisation for debug

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,26 @@
-package client
+package main
+
+import (
+	"challenge/pkg/proto"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"log"
+)
 
 func main() {
-	
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := proto.NewChallengeServiceClient(conn)
+	md := metadata.Pairs("i-am-random-key", "123")
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	url := "https://example.com"
+	data, err := c.MakeShortLink(ctx, &proto.Link{Data: url})
+	fmt.Println(data)
+	fmt.Println(err)
 }
--- pkg/server/MakeShortLink.go
+++ pkg/server/MakeShortLink.go
@@ -1,11 +1,12 @@
 package server
 
 import (
+	"bytes"
 	"challenge/pkg/proto"
 	"context"
 	"fmt"
 	"github.com/spf13/viper"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"strings"
@@ -13,27 +14,38 @@ import (
 
 func (*Server) MakeShortLink(ctx context.Context, in *proto.Link) (*proto.Link, error) {
 	client := &http.Client{}
-	raw := `{"long_url": `
-	raw = raw + in.Data + "}"
-	data := strings.NewReader(raw)
-	req, err := http.NewRequest("POST", "https://api-ssl.bitly.com/v4/shorten", data)
+
+	data := strings.NewReader(fmt.Sprintf(`{"long_url": "%s", "domain": "bit.ly"}`, in.Data))
+	fmt.Println("Data to link: ", data)
+	urlAPI := "https://api-ssl.bitly.com/v4/shorten"
+	req, err := http.NewRequest("POST", urlAPI, data)
 	if err != nil {
-		panic(fmt.Errorf("NewRequest erorr: %v", err))
+		return nil, err
 	}
 	token := viper.GetString("BITLY_OAUTH_TOKEN")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer {%s}", token))
+	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return nil, fmt.Errorf("Responce status not success: %s\n", resp.Status)
+	}
+
 	defer resp.Body.Close()
-	bodyText, err := ioutil.ReadAll(resp.Body)
+	bodyText, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
+		return nil, err
 	}
-	///TODO: incorrect message decode
-	fmt.Printf("%s\n", bodyText)
-	return nil, nil
+
+	v := viper.New()
+	v.SetConfigType("json")
+	v.ReadConfig(bytes.NewBuffer(bodyText))
+	s := v.GetString("link")
+	return &proto.Link{Data: s}, nil
 }
